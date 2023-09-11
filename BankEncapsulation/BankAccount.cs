a72d940f34ef176ba5a14b20a8290135b585fca5
Clifford Myrtil <cliffordhamyrtil@gmail.com>
2023-09-11 13:48:12 -0400

    Completed Bank Encapsulation exercise 1

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double _balance;
+
+        public void Deposit(double amount)
+        {
+            Console.WriteLine($"Simulating a deposit of {amount, 0:c} to your acount!");
+            _balance = amount;
+        }
+        public double GetBalance()
+        { 
+            return _balance;
+        }
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+          BankAccount ba = new BankAccount();
+
+            Console.WriteLine("How much $ would you like to deposit today?");
+            double amountToDeposit = double.Parse(Console.ReadLine());
+
+            ba.Deposit(amountToDeposit);
+           double userBalance = ba.GetBalance();
+
+            Console.WriteLine($"Your current account balance is: {userBalance, 0:c}");
         }
     }
 }
